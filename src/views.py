9db18a03105412a4789b9f2cc91058d6134d0467
user <_@furion.me>
2017-11-02 12:00:47 +0100

    inline embed images

--- src/views.py
+++ src/views.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import binascii
 
 from flask import (
     render_template,
@@ -22,16 +23,20 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def uploader():
+    img_type = request.files['file'].headers[1][1]
     upload = request.files['file'].stream.read()
     assert len(upload) < 5 * 1000 * 1000, "File too large"
 
+    img_b64 = binascii.b2a_base64(upload).decode()
+
+    # import pdb; pdb.set_trace()
     return render_template(
         '_result.html',
         nsfw=nsfw(upload),
         labels=labels(upload),
+        img_b64=img_b64,
+        img_type=img_type,
     )
-
-
 
 if __name__ == '__main__':
     pass
